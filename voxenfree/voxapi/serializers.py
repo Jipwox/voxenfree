c863f3b82dfca37b96068fa40ea8d4192353fce3
JP <johnphilip.cardi@gmail.com>
2022-08-08 18:11:04 -0700

    added serializer and viewset

--- voxenfree/voxapi/serializers.py
+++ voxenfree/voxapi/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from voxcore.models import Product
 
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Product
         fields = ['type', 'name', 'set', 'description', 'price']
--- voxenfree/voxapi/views.py
+++ voxenfree/voxapi/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from voxcore.models import Product
+from rest_framework import viewsets
+from rest_framework import permissions
+from serializers import ProductSerializer
 
-# Create your views here.
+
+class ProductViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]
